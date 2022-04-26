759bbce2353c853f647cfce211898cdcaa6f4a58
Max <maxusan@gmail.com>
2022-04-26 12:42:08 +0300

    main fragment, toolbar and drawer

---
+++ app/src/main/java/banana/code/instagramdownloader/core/database/PostDao.kt
@@ -0,0 +1,7 @@
+package banana.code.instagramdownloader.core.database
+
+import androidx.room.Dao
+
+@Dao
+interface PostDao {
+}
--- app/src/main/java/banana/code/instagramdownloader/ui/MainActivity.kt
+++ app/src/main/java/banana/code/instagramdownloader/ui/MainActivity.kt
@@ -2,9 +2,13 @@ package banana.code.instagramdownloader.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import banana.code.instagramdownloader.R
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: AppViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
